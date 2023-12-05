595cf056610f5d6930c0d2a9358365a043be56a9
DaveCorleto <davide.corleto@fastwebnet.it>
2023-12-05 17:36:54 +0100

    feat post in Milestone 1

--- ex-social-posts-starter/main.js
+++ ex-social-posts-starter/main.js
@@ -70,18 +70,55 @@ const posts = [
 // una volta che ho generato il primo post ciclo il procedimento per ogni elemento di const posts 
 
 
-const = (id, content, media, author, likes, created) Element;
+// seleziono l'elemento che popolerò con il ciclo foreach 
+
+const singlePost = document.getElementById("container");
 
 
-<div class="post">
-            <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
-                    </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">Phil Mangione</div>
-                        <div class="post-meta__time">4 mesi fa</div>
-                    </div>                    
+
+posts.forEach (Element => { 
+
+    //  destructuring 
+
+    const {id, content, media, author, likes, created} = Element;
+    const {name, image} = author;
+
+    console.log (id, content, media, name, image , likes, created);
+
+    const myPost = 
+       `
+        <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src=${image}alt="Phil Mangione">                    
                 </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author"> ${name} </div>
+                    <div class="post-meta__time">${created} </div>
+                </div>                    
             </div>
+        </div>
+        <div class="post__text">${content}</div>
+        <div class="post__image">
+            <img src=${media} alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${likes} </b> persone
+                </div>
+            </div> 
+        </div>
+              `
+
+        container.innerHTML += myPost; 
+});
+
+
